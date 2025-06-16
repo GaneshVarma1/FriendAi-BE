54cc9225860396cff1c4d1ba23aa914916d80c49
Sri Ganesh Shiramshetty <115027993+GaneshVarma1@users.noreply.github.com>
2025-06-16 14:17:54 -0500

    first commit

---
+++ signaling-server.js
@@ -0,0 +1,40 @@
+const { Server } = require('socket.io');
+const http = require('http');
+
+const server = http.createServer();
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('join', (room) => {
+    socket.join(room);
+    socket.to(room).emit('user-joined', socket.id);
+  });
+
+  socket.on('offer', (data) => {
+    socket.to(data.room).emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.to(data.room).emit('answer', data);
+  });
+
+  socket.on('ice-candidate', (data) => {
+    socket.to(data.room).emit('ice-candidate', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Signaling server listening on port ${PORT}`);
+}); 
